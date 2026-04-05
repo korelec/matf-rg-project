5e5111f91d4abe9a26e49312506a56a8ea64a5b1
korelec <korelectea@gmail.com>
2026-04-05 22:40:58 +0200

    load model bandera, glsl, draw_bandera()

--- app/src/MainControler.cpp
+++ app/src/MainControler.cpp
@@ -6,8 +6,9 @@
 
 
 #include <engine/graphics/GraphicsController.hpp>
-#include <engine/platform/PlatformController.hpp>
 #include <engine/graphics/OpenGL.hpp>
+#include <engine/platform/PlatformController.hpp>
+#include <engine/resources/ResourcesController.hpp>
 
 namespace engine::app {
     void MainPlatformEventObserver::on_key(engine::platform::Key) {
@@ -35,14 +36,34 @@ namespace engine::app {
     }
 
     void MainController::begin_draw() {
+        engine::graphics::OpenGL::clear_buffers();
     }
 
+    void MainController::draw_bandera() {
+        //model
+        auto resources            = engine::core::Controller::get<engine::resources::ResourcesController>();
+        auto graphics             = engine::core::Controller::get<engine::graphics::GraphicsController>();
+        resources::Model *bandera = resources->model("bandera");
+        //shader
+        resources::Shader *shader = resources->shader("first");
 
+        shader->use();
+        shader->set_mat4("projection", graphics->projection_matrix());
+        shader->set_mat4("view", graphics->camera()->view_matrix());
+        glm::mat4 model = glm::mat4(1.0f);
+        model           = glm::translate(model, glm::vec3(0.0f, 0.0f, -5.0f));
+        model           = glm::scale(model, glm::vec3(0.3f));
+        shader->set_mat4("model", model);
+        bandera->draw(shader);
+    }
 
     void MainController::draw() {
+        draw_bandera();
     }
 
     void MainController::end_draw() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        platform->swap_buffers();
     }
 
 
